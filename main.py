dd2cc9abe731bcd3ee0d45a8eb494a3463eebbf5
pawan <b419039@iiit-bh.ac.in>
2021-10-12 13:07:43 +0530

    load bot key

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
+from dotenv import load_dotenv
 from os import environ
 import discord
 from discord.ext import commands
-import logging
 
-from discord.ext.commands.core import command
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(
-    filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
-logger.addHandler(handler)
-bot = commands.Bot(command_prefix="$")
-
-@bot.command()
-
-bot.run(environ.get('BOT_KEY'))
+bot=commands.Bot()
+load_dotenv()
+print(environ.get("BOT_KEY"))
