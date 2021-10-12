7a6bc34914ae0d5c45b808fd1de2a99a8cdb2a05
pawan <b419039@iiit-bh.ac.in>
2021-10-12 19:15:02 +0530

    add docstrings

---
+++ info.py
@@ -0,0 +1,12 @@
+REPLY=[
+    "What is it?",
+    "How can I help you?",
+    "Yes, I am listening",
+    "Shhh, only if you need help",
+    "Hi, tell me",
+    "What?",
+    "Something I can do?",
+    "Tell me",
+    "Yo, what's up",
+
+]
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from discord import channel, client
 from discord.ext.commands.core import guild_only
 from dotenv import load_dotenv
 from os import environ
+import random
 import discord
 from discord.ext import commands
 from math import floor
 from jokes import jokes
+from info import REPLY
 load_dotenv()
 
-bot = commands.Bot(command_prefix="%")
 
+intents = discord.Intents.all()
+discord.member = True
+bot = commands.Bot(command_prefix="%",intents=intents)
 # events
 
 
@@ -23,23 +27,31 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    print("member joined")
-    await member.send('Welcome!😀😀😀😀')
+    channel = bot.get_channel(int(environ.get("WELCOME")))
+    await channel.send(f"{member} Welcome my friend😀")
+    
 
 
 @bot.event
 async def on_message(message):
+    """
+    Event triggered on message
+    """
     if(message.author == bot.user):
         return
     if bot.user in message.mentions:
-        await bot.get_channel(message.channel.id).send("🙄,what now?")
+        await bot.get_channel(message.channel.id).send(random.choice(REPLY)+"🤖")
     await bot.process_commands(message)
 
 
 @bot.event
-async def on_member_leave(member):
-    await bot.get_channel(int(environ.get('WELCOME'))).send(member, "left 😔")
-
+async def on_member_remove(member):
+    """
+    Event triggered when member is removed
+    """
+    channel = bot.get_channel(int(environ.get("WELCOME")))
+    await channel.send(f"{member} left the server.😔")
+    
 
 # Commands
 
@@ -48,6 +60,7 @@ async def on_member_leave(member):
 async def delete(ctx, limit=1, before=None, after=None, around=None, oldest_first=None):
     """
     Deletes messages in bulk
+    limit: default is 1
     """
     await ctx.channel.send(f'Deleting {limit} messages')
     await ctx.channel.purge(limit=limit+2, before=before, after=after, around=around, oldest_first=None)
@@ -69,14 +82,22 @@ async def joke(ctx, query="Any"):
 
 @commands.has_permissions(kick_members=True)
 @bot.command()
-async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'User {member} has kicked.')
+async def kick(ctx, user: discord.Member, *, reason=None):
+    """
+        kicks user
+        user: Mention user
+    """
+    await user.kick(reason=reason)
+    await ctx.send(f'User {user} has kicked.')
 
 
 @commands.has_permissions(ban_members=True)
 @bot.command()
-async def ban(ctx, user: discord.Member, *, reason="bad behaviour"):
+async def ban(ctx, user: discord.User, *, reason="bad behaviour"):
+    """
+    bans user
+    user: Mention user
+    """
     await user.ban(reason=reason)
     ban = discord.Embed(
         title=f":boom: Banned {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
@@ -87,6 +108,10 @@ async def ban(ctx, user: discord.Member, *, reason="bad behaviour"):
 
 @bot.command()
 async def unban(ctx, *, member):
+    """
+    unbans user
+    user: Mention user
+    """
     banned_users = await ctx.guild.bans()
     member_name, member_discriminator = member.split('#')
     for ban_entry in banned_users:
